c1b53545b3036d697066d5f53ba68b94c6a8673a
Ezequiel Klausing <ezequielfl@gmail.com>
2022-09-30 23:28:53 -0300

    componente Navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from "../../assets/logo.png"
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt='Logo carrito tienda'></img>
+      <h1>Merchandising</h1>
+      <nav>
+        <a href=''>Remeras</a>
+        <a href=''>Cds</a>
+        <a href=''>gorras</a>
+      </nav>
+      <img src='' alt=''></img>
+    </header>
+  )
+}
+
+
+export default Header
